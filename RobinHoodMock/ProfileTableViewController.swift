f65824800b551bd48297b02873d89e1d279463e3
Dylan Fiedler <dfieds913@gmail.com>
2016-01-05 12:47:42 -0600

    updating profile

--- RobinHoodMock/ProfileTableViewController.swift
+++ RobinHoodMock/ProfileTableViewController.swift
@@ -12,6 +12,8 @@ class ProfileTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //praciticing git commmit adds
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
